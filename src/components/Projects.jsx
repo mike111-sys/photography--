f7c7b6632f9e1fdab2815b5646e4ef47346afd91
Michael Chege <mikechege770@gmail.com>
2024-09-22 17:49:18 +0300

    ssda

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,71 +1,53 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { motion } from 'framer-motion';
+import { PROJECTS } from "../constants"
+import { motion } from "framer-motion"
+
 
 const Projects = () => {
-  const [projects, setProjects] = useState([]);
-
-  // Fetch project data from Strapi
-  useEffect(() => {
-    axios
-      .get('http://localhost:1337/api/projects?populate=*') // Adjust this URL for your Strapi setup
-      .then(response => {
-        setProjects(response.data.data); // Assuming data comes under 'data'
-      })
-      .catch(error => console.error('Error fetching projects:', error));
-  }, []);
-
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <motion.h1
+        <motion.h1
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 1.5 }}
-        className="my-20 text-center text-4xl"
-      >
-        Projects
-      </motion.h1>
-      <div>
-        {projects.map((project, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: -100 }}
-              transition={{ duration: 1 }}
-              className="w-full lg:w-1/4"
-            >
-              <img
-                src={project.attributes.image.data.attributes.url} // Access image URL
-                width={150}
-                height={150}
-                alt={project.attributes.title}
-                className="mb-6 rounded"
-              />
-            </motion.div>
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: 100 }}
-              transition={{ duration: 1 }}
-              className="w-full max-w-xl lg:w-3/4"
-            >
-              <h6 className="mb-2 font-semibold">{project.attributes.title}</h6>
-              <p className="mb-4 text-neutral-400">
-                {project.attributes.description}
-              </p>
-              {project.attributes.technologies.map((tech, index) => (
-                <span
-                  key={index}
-                  className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
-                >
-                  {tech}
+        
+        className="my-20 text-center text-4xl">Projects</motion.h1>
+        <div>
+            {PROJECTS.map((project, index) => (
+                <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+                    <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                    
+                    className="w-full lg:w-1/4">
+<img
+ src={project.image}
+ width={150}
+ height={150}
+  alt={project.title} 
+  className="mb-6 rounded"
+ />
+                </motion.div>
+                <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: 100 }}
+                transition={{ duration: 1  }}
+                
+                className="w-full max-w-xl lg:w-3/4">
+                <h6 className="mb-2 font-semibold">{project.title}</h6>
+                <p className="mb-4 text-neutral-400">{project.description}</p>
+            {project.technologies.map((tech, index) => (
+                <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800" >
+                     {tech}
                 </span>
-              ))}
-            </motion.div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
+            ))}
+                </motion.div>
+                </div>
+            ))}
+    
+        </div>
+        </div>
+  )
+}
 
-export default Projects;
+export default Projects
